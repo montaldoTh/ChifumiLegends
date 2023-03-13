5696f5b1837ed17bb58c4b4cb29703c5dec341ec
Théo <montaldo.to@gmail.com>
2023-03-13 16:35:23 +0100

    Show Result

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const playerValue = document.querySelectorAll("#player-card input");
 const playerCard = document.querySelectorAll("#player-card");
 const ennemieImg = document.querySelector(".background");
 const ennemieTxt = document.querySelector(".ennemi-card .card-title");
+const result = document.querySelector(".result")
+const resultText = document.querySelector(".result-text")
 
 function showEnnemie(){
     if(typeof playerChoice != "undefined"){
@@ -32,51 +34,150 @@ playerCard.forEach(item => {
     item.addEventListener("click", () => {
         if(parseInt(item.childNodes[5].value) == pierre){
             playerChoice = parseInt(item.childNodes[5].value)
-            if(botCard == null){
+            if(botCard == null || botCard == 0){
                 botCard = Math.round(Math.random() * (3 - 1)+ 1)
             }
-            if(botCard != null){
+            if(botCard != null || botCard == 0){
                 showEnnemie()
                 if(playerChoice === pierre && botCard === pierre){
-                    console.log("Egalité");
+                    result.classList.add("egalite")
+                    resultText.innerHTML="Egalité"
+                    resultText.style.display="inline"
+                    setTimeout(()=>{
+                        result.classList.remove("egalite")
+                        resultText.style.display="none"
+                    }, 2000)
+                    setTimeout(() => {
+                        ennemieTxt.innerHTML=" "
+                        ennemieImg.style.background="#061C25"
+                    }, 2500)
+                    botCard = 0;
                 }else if(playerChoice === pierre && botCard  === ciseaux){
-                    console.log("Victoire");
+                    result.classList.add("victoire")
+                    resultText.innerHTML="Victoire"
+                    resultText.style.display="inline"
+                    setTimeout(()=>{
+                        result.classList.remove("victoire")
+                        resultText.style.display="none"
+                    }, 2000)
+                    setTimeout(() => {
+                        ennemieTxt.innerHTML=" "
+                        ennemieImg.style.background="#061C25"
+                    }, 2500)
+                    botCard = 0;
                 }else if(playerChoice === pierre && botCard === feuille){
-                    console.log("Defaite"); 
+                    result.classList.add("defaite")
+                    resultText.innerHTML="Défaite"
+                    resultText.style.display="inline"
+                    setTimeout(()=>{
+                        result.classList.remove("defaite")
+                        resultText.style.display="none"
+                    }, 2000)
+                    setTimeout(() => {
+                        ennemieTxt.innerHTML=" "
+                        ennemieImg.style.background="#061C25"
+                    }, 2500)
+                    botCard = 0;
                 }
             }
             
         }
         else if(parseInt(item.childNodes[5].value) == feuille){
             playerChoice = parseInt(item.childNodes[5].value)
-            if(botCard == null){
+            if(botCard == null || botCard == 0){
                 botCard = Math.round(Math.random() * (3 - 1)+ 1)
             }
-            if(botCard != null){
+            if(botCard != null || botCard == 0){
                 showEnnemie()
                 if(playerChoice === feuille && botCard === pierre){
-                    console.log("Victoire");
+                    result.classList.add("victoire")
+                    resultText.innerHTML="Victoire"
+                    resultText.style.display="inline"
+                    setTimeout(()=>{
+                        result.classList.remove("victoire")
+                        resultText.style.display="none"
+                    }, 2000)
+                    setTimeout(() => {
+                        ennemieTxt.innerHTML=" "
+                        ennemieImg.style.background="#061C25"
+                    }, 2500)
+                    botCard = 0;
                 }else if(playerChoice === feuille && botCard  === ciseaux){
-                    console.log("Defaite"); 
+                    result.classList.add("defaite")
+                    resultText.innerHTML="Défaite"
+                    resultText.style.display="inline"
+                    setTimeout(()=>{
+                        result.classList.remove("defaite")
+                        resultText.style.display="none"
+                    }, 2000)
+                    setTimeout(() => {
+                        ennemieTxt.innerHTML=" "
+                        ennemieImg.style.background="#061C25"
+                    }, 2500)
+                    botCard = 0;
                 }else if(playerChoice === feuille && botCard === feuille){
-                    console.log("Egalité");
+                    result.classList.add("egalite")
+                    resultText.innerHTML="Egalité"
+                    resultText.style.display="inline"
+                    setTimeout(()=>{
+                        result.classList.remove("egalite")
+                        resultText.style.display="none"
+                    }, 2000)
+                    setTimeout(() => {
+                        ennemieTxt.innerHTML=" "
+                        ennemieImg.style.background="#061C25"
+                    }, 2500)
+                    botCard = 0;
                 }
             }
             
         }
         else if(parseInt(item.childNodes[5].value) == ciseaux){
             playerChoice = parseInt(item.childNodes[5].value)
-            if(botCard == null){
+            if(botCard == null || botCard == 0){
                 botCard = Math.round(Math.random() * (3 - 1)+ 1)
             }
-            if(botCard != null){
+            if(botCard != null || botCard == 0){
                 showEnnemie()
                 if(playerChoice === ciseaux && botCard === pierre){
-                    console.log("Defaite"); 
+                    result.classList.add("defaite")
+                    resultText.innerHTML="Défaite"
+                    resultText.style.display="inline"
+                    setTimeout(()=>{
+                        result.classList.remove("defaite")
+                        resultText.style.display="none"
+                    }, 2000)
+                    setTimeout(() => {
+                        ennemieTxt.innerHTML=" "
+                        ennemieImg.style.background="#061C25"
+                    }, 2500)
+                    botCard = 0;
                 }else if(playerChoice === ciseaux && botCard  === ciseaux){
-                    console.log("Egalité");
+                    result.classList.add("egalite")
+                    resultText.innerHTML="Egalité"
+                    resultText.style.display="inline"
+                    setTimeout(()=>{
+                        result.classList.remove("egalite")
+                        resultText.style.display="none"
+                    }, 2000)
+                    setTimeout(() => {
+                        ennemieTxt.innerHTML=" "
+                        ennemieImg.style.background="#061C25"
+                    }, 2500)
+                    botCard = 0;
                 }else if(playerChoice === ciseaux && botCard === feuille){
-                    console.log("Victoire");
+                    result.classList.add("victoire")
+                    resultText.innerHTML="Victoire"
+                    resultText.style.display="inline"
+                    setTimeout(()=>{
+                        result.classList.remove("victoire")
+                        resultText.style.display="none"
+                    }, 2000)
+                    setTimeout(() => {
+                        ennemieTxt.innerHTML=" "
+                        ennemieImg.style.background="#061C25"
+                    }, 2500)
+                    botCard = 0;
                 }
             }
             
